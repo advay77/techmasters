c8b88a600021c51b46ef4e072689eee06a2f894b
advay77 <advayanand37@gmail.com>
2025-05-04 13:13:43 +0530

    Changed these images and hover (Advay)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['via.placeholder.com', 'picsum.photos', 'akm-img-a-in.tosshub.com', 'plus.unsplash.com', 'media.istockphoto.com', 'images.unsplash.com', "assets.aceternity.com"],
+    domains: ['via.placeholder.com', 'picsum.photos', 'akm-img-a-in.tosshub.com', 'plus.unsplash.com', 'media.istockphoto.com', 'images.unsplash.com', "assets.aceternity.com", "drive.google.com"],
   },
 };
 
